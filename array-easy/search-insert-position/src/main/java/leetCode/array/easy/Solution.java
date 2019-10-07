b99e50357219b41b57c422a1ea61945685bed8a9
zpf <zpf@DESKTOP-UJBRQNC>
2019-10-07 19:20:24 +0800

    done

--- array-easy/search-insert-position/src/main/java/leetCode/array/easy/Solution.java
+++ array-easy/search-insert-position/src/main/java/leetCode/array/easy/Solution.java
@@ -3,8 +3,30 @@ package leetCode.array.easy;
 public class Solution {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] nums = {1,3,5,6};
+		int result = searchInsert(nums,7);
+		System.out.print(Integer.valueOf(result));
+		int l=1;
+		int r=2;
+		int m=(l+r)/2;
+		System.out.print(Integer.valueOf(m));
+
 
 	}
-
+	public static int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+        int mid = 0;
+        while(left<right){
+        	mid = (left+right)/2;
+        	if(nums[mid]==target)
+        		return mid;
+        	else if(nums[mid]<target)
+        		left = mid +1;             //此时nums[mid]可以舍弃
+        	else
+        		right = mid;               //此时不能省略，因为除法不是四舍五入，只取整数位，如果舍去则取不到最后一位
+        }
+        
+		return left;
+    }
 }
