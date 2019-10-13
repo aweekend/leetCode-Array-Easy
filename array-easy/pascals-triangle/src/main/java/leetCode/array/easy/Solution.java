cc7d96326ff4c8dced1f3a9b05a3e671a4e30777
zpf <zpf@DESKTOP-UJBRQNC>
2019-10-13 21:16:30 +0800

    tip

--- array-easy/pascals-triangle/src/main/java/leetCode/array/easy/Solution.java
+++ array-easy/pascals-triangle/src/main/java/leetCode/array/easy/Solution.java
@@ -3,6 +3,10 @@ package leetCode.array.easy;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author zpf
+ * 杨辉三角第n行其实是排列组合阶乘公式(C)
+ */
 public class Solution {
 
 	public static void main(String[] args) {
