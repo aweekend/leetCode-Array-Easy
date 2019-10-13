a44b0da8b01e6af81431cd35b2d48add7654179d
zpf <zpf@DESKTOP-UJBRQNC>
2019-10-13 19:26:21 +0800

    create

--- array-easy/maximum-subarray/src/main/java/leetCode/array/easy/Solution.java
+++ array-easy/maximum-subarray/src/main/java/leetCode/array/easy/Solution.java
@@ -8,7 +8,16 @@ public class Solution {
 	}
 	
 	 public int maxSubArray(int[] nums) {
-		 // TODO
-		 return 0;
+		 int tmp = nums[0];                       //设tmp为连续子序列的和，初始值为数组第一个数字
+		 int ans = tmp;                           //ans为最大连续子序列的和
+		 for(int i=0;i<nums.length;i++){
+			 if(tmp>0){                          //如果tmp大于0，则继续向后一个数字相加时，对后一个数字有增益效果，否则不如直接使用后一个数字开始
+				 tmp +=nums[i];
+			 }else{                              //如上面所说，tmp如果小于0，则从后一个数字开始
+				 tmp = nums[i];
+			 }
+			 ans = Math.max(ans, tmp);           //每次循环，ans都取较大的值，则最终ans为最大连续子序列的和
+		 }
+		 return ans;
 	    }
 }
---
+++ array-easy/merge-sorted-array/src/main/java/leetCode/array/easy/Solution.java
@@ -0,0 +1,13 @@
+package leetCode.array.easy;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+		
+    }
+}
